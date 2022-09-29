af8f5c95813c0fb8b092cf12c00042c97f0b3f21
stydfhrl <fahrilsetyoadi@gmail.com>
2022-09-29 16:07:03 +0700

    galeri

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,79 @@
 @extends('layout.main')
 
 @section('content')
-    
+<div class="container-fluid container-gallery">   
+    <div class="gallery-title">
+        <h1>GALLERY</h1>
+    </div>
+    <div class="gallery-card-container">
+        <div class="gallery-card-top">
+            <div class="gallery-card">
+                <div class="gallery-img">
+                    <img src="img/gallery1.jpeg" alt="">
+                </div>
+                <div class="gallery-content">
+                    <h6>COMPANY PROFILE - PT. INSPIRY INDONESIA KONSULTAN</h6>
+                </div>
+            </div>
+            <div class="gallery-card">
+                <div class="gallery-img">
+                    <img src="img/gallery2.jpeg" alt="">
+                </div>
+                <div class="gallery-content">
+                    <h6>IT | TIME MANAGEMENT YANG BAIK</h6>
+                </div>
+            </div>
+            <div class="gallery-card">
+                <div class="gallery-img">
+                    <img src="img/gallery3.jpeg" alt="">
+                </div>
+                <div class="gallery-content">
+                    <h6>TEASER | INSPIRY HADIR DI INDO LIVESTOCK 2022 EXHIBITION</h6>
+                </div>
+            </div>
+            <div class="gallery-card">
+                <div class="gallery-img">
+                    <img src="img/gallery4.jpeg" alt="">
+                </div>
+                <div class="gallery-content">
+                    <h6>IT | MENTAL BAJA</h6>
+                </div>
+            </div>
+        </div>
+        <div class="gallery-card-bottom">
+            <div class="gallery-card">
+                <div class="gallery-img">
+                    <img src="img/gallery5.jpeg" alt="">
+                </div>
+                <div class="gallery-content">
+                    <h6>TEASER LEADERSHIP VIBES 26 MARET 2022 AT HOTEL ASTON PRIORITY SIMATUPANG & CONFERENCE CENTER</h6>
+                </div>
+            </div>
+            <div class="gallery-card">
+                <div class="gallery-img">
+                    <img src="img/gallery6.jpeg" alt="">
+                </div>
+                <div class="gallery-content">
+                    <h6>OBRAS | TIPS SUKSES BISNIS ALAT KESEHATAN EPISODE 2</h6>
+                </div>
+            </div>
+            <div class="gallery-card">
+                <div class="gallery-img">
+                    <img src="img/gallery7.jpeg" alt="">
+                </div>
+                <div class="gallery-content">
+                    <h6>OBRAS | TIPS SUKSES BISNIS ALAT KESEHATAN EPISODE 3</h6>
+                </div>
+            </div>
+            <div class="gallery-card">
+                <div class="gallery-img">
+                    <img src="img/gallery8.jpeg" alt="">
+                </div>
+                <div class="gallery-content">
+                    <h6>OBRAS | TIPS SUKSES DI BISNIS ALAT KESEHATAN PENGALAMAN EPISODE TESTIMONI PT MEGA GLOBAL PRATAMA</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
